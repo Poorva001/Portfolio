4f97c34ebbe9783ca47c9d1d95333e879e39bd73
Poorva001 <122814199+Poorva001@users.noreply.github.com>
2024-04-18 20:57:21 -0400

    minor changes

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menu-btn');
-const navUl = document.querySelector('nav ul');
-
-menuBtn.addEventListener('click', () => {
-  navUl.classList.toggle('hidden');
-});
